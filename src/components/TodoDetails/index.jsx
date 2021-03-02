97956cdc518cba67174fe0a540d26507009e420b
mytoandeptrai <mytoandn@gmail.com>
2021-03-02 19:16:16 +0700

    mytoandeptrai

--- src/components/TodoDetails/index.jsx
+++ src/components/TodoDetails/index.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 
 const TodoDetails = ({todo}) => {
+  const style = {
+    fontWeight: 400,
+  }
   return (
     <div className="todoList__items-below">
-      <p>Details : {todo.details}</p>
+      <p style={style}>Details : {todo.details}</p>
     </div>
   );
 };
